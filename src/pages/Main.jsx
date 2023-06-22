3d29eea2ee52ccd2297d8f8abcea76a715d669a9
rikkaaar <rikkaaar@gmail.com>
2023-06-22 18:43:26 +0300

    - Добавлены favicons - Добавлены ссылки на соц.сети - Добавлен логотип CodeX

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -98,7 +98,16 @@ const Main = () => {
                         <p className={"h2-contacts"}>Телефон:  <Link to={"tel:+79200112255"} className={"contacts__phone"}>+7 (920) 011-22-55</Link></p>
                         <p className={"h2-contacts"}>E-mail:  <Link to={"mailto:codex.ru@mail.ru?subject=Заявка на разработку"} className={"contacts__phone"}>codex.ru@mail.ru</Link></p>
                         <p className={"h2-contacts"}>Юр.адрес:  </p>
-                        <p className={"h2-contacts"}>Ссылки на соц.сети</p>
+                        <p className={"h2-contacts"}>Ссылки на соц.сети:
+                            <ul className="contacts__social social">
+                                <li className="social__item">
+                                    <Link to={"https://api.whatsapp.com/send?phone=79200112255"} target="_blank" className="social__link social__link--whatsapp contacts__social__link--whatsapp" aria-label="Перейти в ватсап"></Link>
+                                </li>
+                                <li className="social__item">
+                                    <Link to={"https://t.me/+79200112255"} target="_blank" className="social__link social__link--telegram contacts__social__link--telegram" aria-label="Перейти в телеграм"></Link>
+                                </li>
+                            </ul>
+                        </p>
                     </div>
                 </div>
                 <div className="contacts__form">
